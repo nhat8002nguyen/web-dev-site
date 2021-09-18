2e6e7d1cec88f37c8514747292679e9d82edfb61
Nhat Nguyen <nv.nhat8002@gmail.com>
2021-09-18 20:04:18 +0700

    update

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
           <span>
             Developed by
             {' '}
-            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
+            <a href="/">JULIUS GUEVARRA</a>
           </span>
         </strong>
       </div>
@@ -32,8 +32,7 @@ const Footer = () => {
       <div className="footer-col-3">
         <strong>
           <span>
-            Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
+            This project &nbsp;
           </span>
         </strong>
       </div>
